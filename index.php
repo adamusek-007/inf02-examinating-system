166be9a8f0fedbf49db16183c85d10da258e6f8b
adamusek-007 <adam.kuczabinski@gmail.com>
2023-04-20 20:03:25 +0200

    Moved PHP code to the start of the page

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+include("connection.php");
+$con = getConnectionToDatabase();
+$stmt = $con->prepare("SELECT * FROM `current_class`;");
+$stmt->execute();
+$curr_class_row = $stmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT, 0);
+$curr_class = $curr_class_row["current_class"];
+if ($curr_class == "Wybierz klasę") {
+    echo "<p>Poproś nauczyciela o wybranie klasy</p>";
+    exit();
+} else {
+    header("Location: ${_SERVER['REMOTE-ADDR']}/examinee/index.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,20 +23,6 @@
 </head>
 
 <body>
-    <?php
-    include("connection.php");
-    $con = getConnectionToDatabase();
-    $stmt = $con->prepare("SELECT * FROM `current_class`;");
-    $stmt->execute();
-    $curr_class_row = $stmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT, 0);
-    $curr_class = $curr_class_row["current_class"];
-    if ($curr_class == "Wybierz klasę") {
-        echo "<p>Poproś nauczyciela o wybranie klasy</p>";
-        exit();
-} else {
-    header("Location: ${_SERVER['REMOTE-ADDR']}/examinee/index.php");
-    }
-    ?>
     <form action="examinee/index.php" method="POST" id="form">
         <label>Wprowadź swój numer po którym będą identyfikowane twoje odpowiedzi</label>
         <input type="number" name="number" required min="1" max="64">
