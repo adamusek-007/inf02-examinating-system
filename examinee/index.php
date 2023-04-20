fb009d8e40b4fb931f263941804b74cccde7f783
adamusek-007 <adam.kuczabinski@gmail.com>
2023-04-20 20:10:54 +0200

    Created functions

--- examinee/index.php
+++ examinee/index.php
@@ -4,40 +4,58 @@ $con = getConnectionToDatabase();
 $stmt = $con->prepare("SELECT * FROM `tasks` ORDER BY RAND() LIMIT 1;");
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $c_answererrererer = $row["c_answer"];
-        ?>
+$c_answer = $row["c_answer"];
+$has_file = $row["has_file"];
+$file_name = $row["file_name"];
+function videoOrImage($has_file, $file_name) {
+    if ($has_file) {
+        if (
+            str_contains(
+                $file_name,
+                'PNG'
+            ) ||
+            str_contains(
+                $file_name,
+                'png'
+            ) ||
+            str_contains(
+                $file_name,
+                'jpg'
+            ) ||
+            str_contains(
+                $file_name,
+                'JPG'
+            )
+        ) {
+            echo "<img width='320' height='auto' src='/files${file_name}' alt='Błąd wczytywania zdjęcia :)'>";
+        } else {
+            echo "<video width='320' height='auto' controls><source src='/files${file_name}' type='video/mp4'></video>";
+        }
+        echo "<br>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odpowiadaj</title>
+    <!-- <link rel="stylesheet" type="text/css" href="style.css" /> -->
+</head>
+
+<body>
+    <form method="POST" id="form">
+
         <label>
             <?= $row["content"] ?>
         </label>
-            <?php
-            $has_file = $row["has_file"];
-            $file_name = $row["file_name"];
-            if ($has_file) {
-                if (
-                    str_contains(
-                        $file_name,
-                        'PNG'
-                    ) ||
-                    str_contains(
-                        $file_name,
-                        'png'
-                    ) ||
-                    str_contains(
-                        $file_name,
-                        'jpg'
-                    ) ||
-                    str_contains(
-                        $file_name,
-                        'JPG'
-                    )
-                ) {
-                    echo "<img width='320' height='auto' src='/files${file_name}' alt='Błąd wczytywania zdjęcia :)'>";
-                } else {
-                    echo "<video width='320' height='auto' controls><source src='/files${file_name}' type='video/mp4'></video>";
-                }
-                echo "<br>";
-            }
-            ?>
+        <?php
+            videoOrImage($has_file, $file_name);
+        ?>
         <div>
             <fieldset id="user-answer">
                 <legend>Wybierz odpowiedź</legend>
@@ -55,6 +73,8 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
                     value="<?= $row["answer_4"] ?>" onclick='elemele(this)'><br>
             </fieldset>
         </div>
+        <div id="q-id-input" class="display-none"><?php echo $row["id"]; ?></div>
+        <div id="correct-answer" class="display-none"><?php echo $c_answer ?></div>
     </form>
 </body>
 <script src="javascript.js"></script>
