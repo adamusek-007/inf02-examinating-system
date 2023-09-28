9e52481a377bd8cd609caea73a05ca24cc596faf
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-28 06:16:34 +0200

    Create Docker files and edit comopse.yaml

--- operator/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-include("operator-backend.php");
-include("../connection.php");
--- operator/s_update-task.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("../connection.php");
-$taskId = $_REQUEST["par1"];
-$answerId = $_REQUEST["par2"];
-$con = getConnectionToDatabase();
-$stmt = $con->prepare("UPDATE tasks SET `c_answer`='answer_{$answerId}' WHERE id = {$taskId}");
-$stmt->execute();
