2ae31af1617868b12249e7c775305451f8be9a3f
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-28 07:12:22 +0200

    Adjust connections to the rest of the code

--- app/connection.php
+++ app/connection.php
@@ -1,26 +1,41 @@
 <?php
 
-const DB_HOST = "127.0.0.1";
-const DB_USER = "operator";
-const DB_PASSWORD = "xsw2#EDC";
+const DB_HOST = "mysql-db";
+const DB_USER = "root";
+const DB_PASSWORD = "root";
 const DB_NAME = "zadania";
+define('DB_PORT', 3306);
 /**
  * Connects to Database
  */
-function getConnectionToDatabase(): PDO
-{
+/**
+* Creates PDO connection Data Source Name (DSN).
+* 
+* @return string The PDO DSN.
+*/
+function create_dsn() {
+    $dsn_format = "mysql:host=%s;dbname=%s;port=%s;charset=utf8";
+    return sprintf($dsn_format, DB_HOST, DB_NAME, DB_PORT);
+ }
+ 
+ /**
+ * Gets a PDO database connection.
+ *
+ * @return PDO|null A PDO database connection object, or null on failure.
+ */
+ function getConnectionToDatabase(): PDO
+ {
     static $pdo;
+    
     if (!$pdo) {
-        try {$pdo = new PDO(
-            sprintf("mysql:host=%s;dbname=%s;charset=UTF8", DB_HOST, DB_NAME),
-            DB_USER,
-            DB_PASSWORD,
-            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-        );} catch(PDOException) {
-            echo "Nie można połączyć się z bazą danych.";
-            exit();
+        try {
+            $pdo = new PDO(create_dsn(),DB_USER, DB_PASSWORD);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo $exception;
+            exit;
         }
     }
     return $pdo;
-}
+ }
 ?>
--- app/examinee-conn.php
+++ app/examinee-conn.php
@@ -1,23 +1,38 @@
 <?php
 
-const DB_HOST = "127.0.0.1";
-const DB_USER = "examinee";
-const DB_PASSWORD = "zaq1@WSX";
+const DB_HOST = "mysql-db";
+const DB_USER = "root";
+const DB_PASSWORD = "root";
 const DB_NAME = "zadania";
-function getConnectionToDatabase(): PDO
-{
+define('DB_PORT', 3306);
+/**
+* Creates PDO connection Data Source Name (DSN).
+* 
+* @return string The PDO DSN.
+*/
+function create_dsn() {
+    $dsn_format = "mysql:host=%s;dbname=%s;port=%s;charset=utf8";
+    return sprintf($dsn_format, DB_HOST, DB_NAME, DB_PORT);
+ }
+ 
+ /**
+ * Gets a PDO database connection.
+ *
+ * @return PDO|null A PDO database connection object, or null on failure.
+ */
+ function getConnectionToDatabase(): PDO
+ {
     static $pdo;
+    
     if (!$pdo) {
-        try {$pdo = new PDO(
-            sprintf("mysql:host=%s;dbname=%s;charset=UTF8", DB_HOST, DB_NAME),
-            DB_USER,
-            DB_PASSWORD,
-            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] 
-        ); } catch(PDOException) {
-            echo "Nie można połączyć się z bazą danych.";
+        try {
+            $pdo = new PDO(create_dsn(),DB_USER, DB_PASSWORD);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo $exception;
             exit;
         }
     }
     return $pdo;
-}
+ }
 ?>
